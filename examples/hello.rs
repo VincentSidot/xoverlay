7ef5b9a9a0ae09ef3c40f186904937415a881efc
Vincent S. <vs38190@gmail.com>
2024-06-10 21:58:28 +0200

    [UPDATE] Update on example

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,22 +1,21 @@
+use x11rb::protocol::xproto::KeyButMask;
 use xoverlay::{
     event::Event,
+    key::{Key, KeyRef},
     shape::{
-        coord::{Anchor, Coord, Size}, Arc, Rectangle, Shape
-    }, Color, Mapping, Overlay,
-    x11rb::rust_connection::RustConnection
+        coord::{Anchor, Coord, Size},
+        Rectangle, Shape,
+    },
+    x11rb::rust_connection::RustConnection,
+    Color, Drawable, Mapping, Overlay,
 };
 
-use std::{
-    env,
-    error::Error
-};
+use std::{env, error::Error, rc::Rc, sync::Arc};
 
-fn main() -> Result<(), Box<dyn Error>>{
-    
+fn main() -> Result<(), Box<dyn Error>> {
     // Fetch window from argument
     let args: Vec<String> = env::args().collect();
     let window = if args.len() > 1 {
-
         let arg = &args[1];
         let num = if let Some(num) = arg.strip_prefix("0x") {
             num
@@ -30,53 +29,95 @@ fn main() -> Result<(), Box<dyn Error>>{
         Err("No window provided")?
     };
 
-    println!("Window: {:#x}", window);
+    // println!("Window: {:#x}", window);
 
     // Initialize the overlay
-    let mut overlay = Overlay::init(
-        window,
-        &Mapping::FullScreen, 
-        None
-    )?;
+    let mut overlay = Overlay::init(window, &Mapping::FullScreen, None)?;
+
+    // Display parent and overlay window ids
+    println!("Parent window: {:#x}", overlay.parent().id());
+    println!("Overlay window: {:#x}", overlay.window().id());
 
     // Create rectangles
-    let rec: Vec<Box<dyn Shape<RustConnection>>> = vec![
-        Rectangle::fill(
-            Anchor::Center,
-            Coord::new(0.25, 0.5),
-            Size::new(0.25, 0.25),
-            Color::BLUE,
-        )?,
-        Arc::filled_circle(
-            Anchor::Center,
-            Coord::new(0.25, 0.5),
-            0.2,
-            Color::TRANSPARENT,
-        )?,
-        // Rectangle::fill(
-        //     Anchor::NorthEast,
-        //     Coord::new(1.0, 0.0),
-        //     Size::new(0.2, 1.0),
-        //     Color::RED,
-        // )?,
-    ];
+    let rec = Rectangle::fill(
+        Anchor::Center,
+        Coord::new(0.5, 0.5),
+        Size::new(0.1, 0.1),
+        Color::BLUE,
+    )?;
+
+    // let to_draw : Vec<&dyn Shape<RustConnection>> = vec![
+    //     &rec,
+    //     // Rectangle::fill(
+    //     //     Anchor::NorthEast,
+    //     //     Coord::new(1.0, 0.0),
+    //     //     Size::new(0.2, 1.0),
+    //     //     Color::RED,
+    //     // )?,
+    // ];
 
     // Add the rectangles to the overlay
-    overlay
-        .add_shapes(rec)
-        .event_loop(|_, event| {
-            if event == Event::Unkown {
-                return
+    overlay.add_shape(rec.clone()).event_loop(|_, event| {
+        match event {
+            Event::KeyPress(Key { key, modifiers }) => {
+                let mut rec = rec.borrow_mut();
+                const STEP: f32 = 0.01;
+                let is_resize = if modifiers.contains(KeyButMask::CONTROL) {
+                    true
+                } else {
+                    false
+                };
+                let mut size = *rec.size();
+                let mut pos = *rec.position();
+                match key {
+                    KeyRef::ArrowUp => {
+                        if is_resize {
+                            size.y += STEP;
+                        } else {
+                            pos.y -= STEP;
+                        }
+                    }
+                    KeyRef::ArrowDown => {
+                        if is_resize {
+                            size.y -= STEP;
+                        } else {
+                            pos.y += STEP;
+                        }
+                    }
+                    KeyRef::ArrowLeft => {
+                        if is_resize {
+                            size.x -= STEP;
+                        } else {
+                            pos.x -= STEP;
+                        }
+                    }
+                    KeyRef::ArrowRight => {
+                        if is_resize {
+                            size.x += STEP;
+                        } else {
+                            pos.x += STEP;
+                        }
+                    }
+                    _ => return None, // Ignore other keys
+                }
+                rec.set_position(pos);
+                rec.set_size(size);
+
+                // Redraw the overlay
+                Some(Event::Redraw)
             }
-            // Print the event
-            println!("Event: {:?}", event);
-        })?;
+            _ => {
+                // Print the event
+                println!("Event: {:?}", event);
+                None
+            }
+        }
+    })?;
 
     // loop {
     //     let (event, seq) = overlay.conn.wait_for_event_with_sequence()?;
     //     println!("[{}] Event: {:?}", seq, event);
     // }
-    
-    Ok(())
 
+    Ok(())
 }
